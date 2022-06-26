22b3d1970a8e1231e778d34bc78eb72c5925c0eb
Vincent <vpark45@gmail.com>
2022-06-26 13:08:28 +0000

    Add generators for Maybe, Either

--- Addition.hs
+++ Addition.hs
@@ -62,3 +62,21 @@ genThreeple = do
   b <- arbitrary
   c <- arbitrary
   return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b)
+          => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a)) ]
